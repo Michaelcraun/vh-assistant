68cc92e81484a3baa4c11bbc2c346e5f33757e27
Michael Craun <michael.craun@gmail.com>
2022-09-28 17:17:44 -0400

    Initial Commit

---
+++ Shared/VH_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  VH_AssistantApp.swift
+//  Shared
+//
+//  Created by Michael Craun on 9/28/22.
+//
+
+import SwiftUI
+
+@main
+struct VH_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
